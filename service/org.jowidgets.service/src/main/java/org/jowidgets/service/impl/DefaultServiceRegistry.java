5e0c56ac8edac5f6d35f0fadac26b04686cd76b0
herr.grossmann <herr.grossmann@gmx.de>
2011-06-09 09:38:11 +0000

    added service decorator plugin

--- service/org.jowidgets.service/src/main/java/org/jowidgets/service/impl/DefaultServiceRegistry.java
+++ service/org.jowidgets.service/src/main/java/org/jowidgets/service/impl/DefaultServiceRegistry.java
@@ -29,27 +29,41 @@
 package org.jowidgets.service.impl;
 
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.ServiceLoader;
 import java.util.Set;
 
+import org.jowidgets.service.api.IServiceDecoratorPlugin;
 import org.jowidgets.service.api.IServiceId;
 import org.jowidgets.service.api.IServiceProvider;
 import org.jowidgets.service.api.IServiceRegistry;
 import org.jowidgets.util.Assert;
+import org.jowidgets.util.IDecorator;
 
 public class DefaultServiceRegistry implements IServiceRegistry, IServiceProvider {
 
 	private final Map<IServiceId<?>, Object> services;
+	private final Map<IServiceId<?>, Object> decoratedServices;
+	private final List<IServiceDecoratorPlugin> serviceDecorators;
 
 	public DefaultServiceRegistry() {
 		this.services = new HashMap<IServiceId<?>, Object>();
+		this.decoratedServices = new HashMap<IServiceId<?>, Object>();
+		this.serviceDecorators = getRegisteredServicesDecorators();
 	}
 
-	@SuppressWarnings("unchecked")
+	@Override
+	public synchronized void addServiceDecorator(final IServiceDecoratorPlugin serviceDecoratorPlugin) {
+		serviceDecorators.add(serviceDecoratorPlugin);
+		decoratedServices.clear();
+	}
+
 	@Override
 	public final <SERVICE_TYPE> SERVICE_TYPE get(final IServiceId<SERVICE_TYPE> id) {
-		Assert.paramNotNull(id, "id");
-		return (SERVICE_TYPE) services.get(id);
+		return getDecoratedService(id);
 	}
 
 	@Override
@@ -63,6 +77,41 @@ public class DefaultServiceRegistry implements IServiceRegistry, IServiceProvide
 	@Override
 	public final Set<IServiceId<?>> getAvailableServices() {
 		return services.keySet();
+	}
+
+	@SuppressWarnings("unchecked")
+	private <SERVICE_TYPE> SERVICE_TYPE getDecoratedService(final IServiceId<SERVICE_TYPE> id) {
+		Assert.paramNotNull(id, "id");
+		SERVICE_TYPE result = (SERVICE_TYPE) decoratedServices.get(id);
+		if (result == null) {
+			result = (SERVICE_TYPE) services.get(id);
+			if (result != null) {
+				for (final IServiceDecoratorPlugin serviceDecorator : new LinkedList<IServiceDecoratorPlugin>(serviceDecorators)) {
+					final IDecorator<Object> defaultDecorator = serviceDecorator.getDefaultDecorator();
+					if (defaultDecorator != null) {
+						defaultDecorator.decorate(result);
+					}
+					final IDecorator<SERVICE_TYPE> decorator = serviceDecorator.getDecorator(id.getServiceType());
+					if (decorator != null) {
+						result = decorator.decorate(result);
+					}
+				}
+			}
+			decoratedServices.put(id, result);
+		}
+		return result;
+	}
+
+	private List<IServiceDecoratorPlugin> getRegisteredServicesDecorators() {
+		final List<IServiceDecoratorPlugin> result = new LinkedList<IServiceDecoratorPlugin>();
+		final ServiceLoader<IServiceDecoratorPlugin> widgetServiceLoader = ServiceLoader.load(IServiceDecoratorPlugin.class);
+		if (widgetServiceLoader != null) {
+			final Iterator<IServiceDecoratorPlugin> iterator = widgetServiceLoader.iterator();
+			while (iterator.hasNext()) {
+				result.add(iterator.next());
+			}
+		}
+		return result;
 	}
 
 }
