6196c071f10297fd1240667e79b47cd5c3e13af2
h.westphal <h.westphal@gmx.de>
2011-06-24 09:43:50 +0000

    http message api impl

---
+++ service/org.jowidgets.message.impl.http.client/src/main/java/org/jowidgets/message/impl/http/client/MessageBrokerBuilder.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2011, H.Westphal
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ * * Neither the name of the jo-widgets.org nor the
+ *   names of its contributors may be used to endorse or promote products
+ *   derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL jo-widgets.org BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+ * DAMAGE.
+ */
+package org.jowidgets.message.impl.http.client;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.jowidgets.util.Assert;
+
+public final class MessageBrokerBuilder {
+
+	private final Object brokerId;
+
+	private String url = "http://localhost:8080/";
+	private HttpClient httpClient;
+
+	public MessageBrokerBuilder(final Object brokerId) {
+		Assert.paramNotNull(brokerId, "brokerId");
+		this.brokerId = brokerId;
+	}
+
+	public MessageBrokerBuilder setUrl(final String url) {
+		Assert.paramNotNull(url, "url");
+		this.url = url;
+		return this;
+	}
+
+	public MessageBrokerBuilder setHttpClient(final HttpClient httpClient) {
+		Assert.paramNotNull(httpClient, "httpClient");
+		this.httpClient = httpClient;
+		return this;
+	}
+
+	public IMessageBroker build() {
+		return new MessageBroker(brokerId, url, httpClient == null ? new DefaultHttpClient() : httpClient);
+	}
+
+}
