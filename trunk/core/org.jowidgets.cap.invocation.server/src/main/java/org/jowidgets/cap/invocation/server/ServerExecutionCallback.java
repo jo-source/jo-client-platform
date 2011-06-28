15d37b9bf8ba9718ec18126d7658c690c5cecbdb
herr.grossmann@gmx.de <herr.grossmann@gmx.de@241b2d79-ac52-3d8b-96e9-93e4125b0f3f>
2011-06-28 15:33:15 +0000

    exchanged timer task by scheduledExecuterService

--- trunk/core/org.jowidgets.cap.invocation.server/src/main/java/org/jowidgets/cap/invocation/server/ServerExecutionCallback.java
+++ trunk/core/org.jowidgets.cap.invocation.server/src/main/java/org/jowidgets/cap/invocation/server/ServerExecutionCallback.java
@@ -31,8 +31,8 @@ package org.jowidgets.cap.invocation.server;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Set;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import org.jowidgets.cap.common.api.execution.IExecutionCallback;
@@ -51,9 +51,9 @@ final class ServerExecutionCallback implements IExecutionCallback {
 	private final IInterimResponseCallback<Progress> interimResponseCallback;
 	private final Set<IExecutionCallbackListener> executionCallbackListeners;
 
+	private final ScheduledExecutorService scheduledExecutorService;
 	private final long progressDelay;
-	private final Timer timer;
-	private final AtomicBoolean isTimerSheduled;
+	private final AtomicBoolean isProgressScheduled;
 
 	private boolean canceled;
 	private Integer totalStepCount;
@@ -62,13 +62,16 @@ final class ServerExecutionCallback implements IExecutionCallback {
 	private boolean finished;
 
 	ServerExecutionCallback(
+		final ScheduledExecutorService scheduledExecutorService,
 		final long progressDelay,
 		final IInvocationCallback<Object> invocationCallback,
 		final IInterimResponseCallback<Progress> interimResponseCallback) {
 
+		Assert.paramNotNull(scheduledExecutorService, "scheduledExecutorService");
 		Assert.paramNotNull(invocationCallback, "invocationCallback");
 		Assert.paramNotNull(interimResponseCallback, "interimResponseCallback");
 		this.progressDelay = progressDelay;
+		this.scheduledExecutorService = scheduledExecutorService;
 		this.invocationCallback = invocationCallback;
 		this.interimResponseCallback = interimResponseCallback;
 		this.executionCallbackListeners = new HashSet<IExecutionCallbackListener>();
@@ -87,8 +90,7 @@ final class ServerExecutionCallback implements IExecutionCallback {
 			}
 		});
 
-		this.timer = new Timer();
-		this.isTimerSheduled = new AtomicBoolean(false);
+		this.isProgressScheduled = new AtomicBoolean(false);
 
 	}
 
@@ -136,12 +138,16 @@ final class ServerExecutionCallback implements IExecutionCallback {
 
 	@Override
 	public IExecutionCallback createSubExecution(final int stepProportion, final boolean cancelable) {
-		return new ServerExecutionCallback(progressDelay, invocationCallback, new IInterimResponseCallback<Progress>() {
-			@Override
-			public void response(final Progress response) {
-				//TODO implement sub progress
-			}
-		});
+		return new ServerExecutionCallback(
+			scheduledExecutorService,
+			progressDelay,
+			invocationCallback,
+			new IInterimResponseCallback<Progress>() {
+				@Override
+				public void response(final Progress response) {
+					//TODO implement sub progress
+				}
+			});
 	}
 
 	@Override
@@ -157,15 +163,15 @@ final class ServerExecutionCallback implements IExecutionCallback {
 	}
 
 	private void setDirty() {
-		if (!isTimerSheduled.getAndSet(true)) {
-			timer.schedule(new TimerTask() {
+		if (!isProgressScheduled.getAndSet(true)) {
+			scheduledExecutorService.schedule(new Runnable() {
 				@Override
 				public void run() {
-					isTimerSheduled.set(false);
+					isProgressScheduled.set(false);
 					final Progress progress = new Progress(totalStepCount, totalWorked, description, finished);
 					interimResponseCallback.response(progress);
 				}
-			}, progressDelay);
+			}, progressDelay, TimeUnit.MILLISECONDS);
 		}
 	}
 }
