5e704efd3a8baccc2b4c0af07ca4c6b5205a2733
herr.grossmann <herr.grossmann@gmx.de>
2011-07-23 08:54:42 +0000

    added menu to change header display format

--- core/org.jowidgets.cap.ui/src/main/java/org/jowidgets/cap/ui/impl/widgets/BeanTableImpl.java
+++ core/org.jowidgets.cap.ui/src/main/java/org/jowidgets/cap/ui/impl/widgets/BeanTableImpl.java
@@ -28,14 +28,22 @@
 
 package org.jowidgets.cap.ui.impl.widgets;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import org.jowidgets.api.command.IAction;
 import org.jowidgets.api.command.IExecutionContext;
 import org.jowidgets.api.convert.IStringObjectConverter;
+import org.jowidgets.api.model.item.IMenuItemModel;
 import org.jowidgets.api.model.item.IMenuModel;
+import org.jowidgets.api.model.item.IRadioItemModel;
 import org.jowidgets.api.toolkit.Toolkit;
 import org.jowidgets.api.widgets.IPopupMenu;
 import org.jowidgets.api.widgets.ITable;
 import org.jowidgets.cap.ui.api.CapUiToolkit;
+import org.jowidgets.cap.ui.api.attribute.DisplayFormat;
 import org.jowidgets.cap.ui.api.attribute.IAttribute;
 import org.jowidgets.cap.ui.api.bean.IBeanProxy;
 import org.jowidgets.cap.ui.api.command.ICapActionFactory;
@@ -47,6 +55,8 @@ import org.jowidgets.cap.ui.api.widgets.IBeanTableBluePrint;
 import org.jowidgets.cap.ui.api.widgets.IBeanTableSettingsDialog;
 import org.jowidgets.cap.ui.api.widgets.ICapApiBluePrintFactory;
 import org.jowidgets.common.types.Modifier;
+import org.jowidgets.common.types.Position;
+import org.jowidgets.common.widgets.controler.IItemStateListener;
 import org.jowidgets.common.widgets.controler.ITableCellEditEvent;
 import org.jowidgets.common.widgets.controler.ITableCellPopupDetectionListener;
 import org.jowidgets.common.widgets.controler.ITableCellPopupEvent;
@@ -58,7 +68,9 @@ import org.jowidgets.tools.command.ExecutionContextWrapper;
 import org.jowidgets.tools.controler.TableCellEditorAdapter;
 import org.jowidgets.tools.controler.TableColumnAdapter;
 import org.jowidgets.tools.model.item.MenuModel;
+import org.jowidgets.tools.model.item.SeparatorItemModel;
 import org.jowidgets.tools.widgets.wrapper.TableWrapper;
+import org.jowidgets.util.EmptyCheck;
 import org.jowidgets.util.IDecorator;
 import org.jowidgets.util.ITypedKey;
 
@@ -68,6 +80,11 @@ final class BeanTableImpl<BEAN_TYPE> extends TableWrapper implements IBeanTable<
 	private final IMenuModel cellPopupMenuModel;
 	private final IMenuModel headerPopupMenuModel;
 	private final IMenuModel tablePopupMenuModel;
+	private final IPopupMenu columnPopupMenu;
+	private final boolean hasDefaultMenus;
+	private final Map<Integer, IMenuModel> headerFormatMenus;
+	private final Map<Integer, IMenuModel> contentFormatMenus;
+	private final List<IMenuItemModel> tempHeaderMenuItems;
 
 	private IBeanTableSettingsDialog settingsDialog;
 	private ITableCellPopupEvent currentCellEvent;
@@ -76,6 +93,10 @@ final class BeanTableImpl<BEAN_TYPE> extends TableWrapper implements IBeanTable<
 	BeanTableImpl(final ITable table, final IBeanTableBluePrint<BEAN_TYPE> bluePrint) {
 		super(table);
 		this.model = bluePrint.getModel();
+		this.hasDefaultMenus = bluePrint.hasDefaultMenus();
+		this.headerFormatMenus = createHeaderFormatMenus(model, hasDefaultMenus);
+		this.contentFormatMenus = createContentFormatMenus(model, hasDefaultMenus);
+		this.tempHeaderMenuItems = new LinkedList<IMenuItemModel>();
 		this.cellPopupMenuModel = new MenuModel();
 		this.headerPopupMenuModel = new MenuModel();
 		this.tablePopupMenuModel = new MenuModel();
@@ -123,7 +144,7 @@ final class BeanTableImpl<BEAN_TYPE> extends TableWrapper implements IBeanTable<
 		});
 
 		//add column popup menu
-		final IPopupMenu columnPopupMenu = table.createPopupMenu();
+		this.columnPopupMenu = table.createPopupMenu();
 		columnPopupMenu.setModel(headerPopupMenuModel);
 		headerPopupMenuModel.addDecorator(createDecorator(true));
 		table.addTableColumnPopupDetectionListener(new ITableColumnPopupDetectionListener() {
@@ -131,7 +152,7 @@ final class BeanTableImpl<BEAN_TYPE> extends TableWrapper implements IBeanTable<
 			public void popupDetected(final ITableColumnPopupEvent event) {
 				if (columnPopupMenu.getChildren().size() > 0) {
 					currentColumnEvent = event;
-					columnPopupMenu.show(event.getPosition());
+					showHeaderPopupMenu(event.getPosition(), event.getColumnIndex());
 				}
 			}
 		});
@@ -139,6 +160,77 @@ final class BeanTableImpl<BEAN_TYPE> extends TableWrapper implements IBeanTable<
 		table.addTableCellEditorListener(new TableCellEditorListener());
 		table.addTableColumnListener(new TableSortColumnListener());
 
+	}
+
+	private static Map<Integer, IMenuModel> createHeaderFormatMenus(final IBeanTableModel<?> model, final boolean hasDefaultMenus) {
+		final Map<Integer, IMenuModel> result = new HashMap<Integer, IMenuModel>();
+		if (hasDefaultMenus) {
+			for (int columnIndex = 0; columnIndex < model.getColumnCount(); columnIndex++) {
+				final IAttribute<Object> attribute = model.getAttribute(columnIndex);
+				if (!EmptyCheck.isEmpty(attribute.getLabelLong())) {
+					final IMenuModel menuModel = new MenuModel("Header format");
+					result.put(Integer.valueOf(columnIndex), menuModel);
+					final IRadioItemModel shortRadioItem = menuModel.addRadioItem(DisplayFormat.SHORT.getName());
+					final IRadioItemModel longRadioItem = menuModel.addRadioItem(DisplayFormat.LONG.getName());
+					if (attribute.getLabelDisplayFormat() == DisplayFormat.SHORT
+						|| attribute.getLabelDisplayFormat() == DisplayFormat.DEFAULT) {
+						shortRadioItem.setSelected(true);
+					}
+					else {
+						longRadioItem.setSelected(true);
+					}
+					shortRadioItem.addItemListener(new IItemStateListener() {
+						@Override
+						public void itemStateChanged() {
+							if (shortRadioItem.isSelected()) {
+								attribute.setLabelDisplayFormat(DisplayFormat.SHORT);
+							}
+						}
+					});
+					longRadioItem.addItemListener(new IItemStateListener() {
+						@Override
+						public void itemStateChanged() {
+							if (longRadioItem.isSelected()) {
+								attribute.setLabelDisplayFormat(DisplayFormat.LONG);
+							}
+						}
+					});
+				}
+			}
+		}
+		return result;
+	}
+
+	private static Map<Integer, IMenuModel> createContentFormatMenus(final IBeanTableModel<?> model, final boolean hasDefaultMenus) {
+		final Map<Integer, IMenuModel> result = new HashMap<Integer, IMenuModel>();
+
+		return result;
+	}
+
+	private void showHeaderPopupMenu(final Position position, final int columnIndex) {
+		if (hasDefaultMenus) {
+			for (final IMenuItemModel tempMenuItem : new LinkedList<IMenuItemModel>(tempHeaderMenuItems)) {
+				tempHeaderMenuItems.remove(tempMenuItem);
+				headerPopupMenuModel.removeItem(tempMenuItem);
+			}
+			final int headerFormatIndex = 3;
+			final IMenuItemModel separator = new SeparatorItemModel();
+			final IMenuModel contentFormatMenu = contentFormatMenus.get(columnIndex);
+			final IMenuModel headerFormatMenu = headerFormatMenus.get(columnIndex);
+			if (headerFormatMenu != null || contentFormatMenu != null) {
+				headerPopupMenuModel.addItem(headerFormatIndex, separator);
+				tempHeaderMenuItems.add(separator);
+			}
+			if (contentFormatMenu != null) {
+				headerPopupMenuModel.addItem(headerFormatIndex, contentFormatMenu);
+				tempHeaderMenuItems.add(contentFormatMenu);
+			}
+			if (headerFormatMenu != null) {
+				headerPopupMenuModel.addItem(headerFormatIndex, headerFormatMenu);
+				tempHeaderMenuItems.add(headerFormatMenu);
+			}
+		}
+		columnPopupMenu.show(position);
 	}
 
 	@Override
