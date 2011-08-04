b85ffa38bad51fb6ae1ffb4a46724c615656bff1
h.westphal <h.westphal@gmx.de>
2011-08-04 16:14:04 +0000

    add optional granted authorities (e.g. for user roles or rights)

--- service/org.jowidgets.security/src/main/java/org/jowidgets/security/tools/DefaultPrincipal.java
+++ service/org.jowidgets.security/src/main/java/org/jowidgets/security/tools/DefaultPrincipal.java
@@ -29,30 +29,45 @@
 package org.jowidgets.security.tools;
 
 import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
 
 public final class DefaultPrincipal implements Serializable {
 
 	private static final long serialVersionUID = 7568678347480658843L;
 
 	private final String username;
+	private final Set<String> grantedAuthorities = new HashSet<String>();
 
 	public DefaultPrincipal(final String username) {
 		this.username = username;
+	}
+
+	public DefaultPrincipal(final String username, final Collection<String> grantedAuthorities) {
+		this(username);
+		this.grantedAuthorities.addAll(grantedAuthorities);
 	}
 
 	public String getUsername() {
 		return username;
 	}
 
+	public Set<String> getGrantedAuthorities() {
+		return Collections.unmodifiableSet(grantedAuthorities);
+	}
+
 	@Override
 	public String toString() {
-		return "DefaultPrincipal [username=" + username + "]";
+		return "DefaultPrincipal [username=" + username + ", grantedAuthorities=" + grantedAuthorities + "]";
 	}
 
 	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
+		result = prime * result + ((grantedAuthorities == null) ? 0 : grantedAuthorities.hashCode());
 		result = prime * result + ((username == null) ? 0 : username.hashCode());
 		return result;
 	}
@@ -65,10 +80,18 @@ public final class DefaultPrincipal implements Serializable {
 		if (obj == null) {
 			return false;
 		}
-		if (getClass() != obj.getClass()) {
+		if (!(obj instanceof DefaultPrincipal)) {
 			return false;
 		}
 		final DefaultPrincipal other = (DefaultPrincipal) obj;
+		if (grantedAuthorities == null) {
+			if (other.grantedAuthorities != null) {
+				return false;
+			}
+		}
+		else if (!grantedAuthorities.equals(other.grantedAuthorities)) {
+			return false;
+		}
 		if (username == null) {
 			if (other.username != null) {
 				return false;
