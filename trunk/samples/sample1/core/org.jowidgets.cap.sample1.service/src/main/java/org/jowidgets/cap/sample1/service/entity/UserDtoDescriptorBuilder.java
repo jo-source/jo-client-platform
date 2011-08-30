d1dcc816710e88c19eface83ab0e6014def754bc
herr.grossmann@gmx.de <herr.grossmann@gmx.de@241b2d79-ac52-3d8b-96e9-93e4125b0f3f>
2011-08-30 13:28:30 +0000

    changed value range for example

--- trunk/samples/sample1/core/org.jowidgets.cap.sample1.service/src/main/java/org/jowidgets/cap/sample1/service/entity/UserDtoDescriptorBuilder.java
+++ trunk/samples/sample1/core/org.jowidgets.cap.sample1.service/src/main/java/org/jowidgets/cap/sample1/service/entity/UserDtoDescriptorBuilder.java
@@ -63,6 +63,7 @@ public class UserDtoDescriptorBuilder extends BeanDtoDescriptorBuilder {
 		propertyBp = addProperty(IUser.LANGUAGES_PROPERTY);
 		propertyBp.setLabel("Languages").setDescription("The languages the user speaks");
 		propertyBp.setElementValueType(String.class).setSortable(false);
+		propertyBp.setValueRange(false, "English", "German", "Italian", "Spanish");
 
 		propertyBp = addProperty(IUser.ADMIN_PROPERTY);
 		propertyBp.setLabel("Admin").setDescription("Determines if the user is an administrator");
