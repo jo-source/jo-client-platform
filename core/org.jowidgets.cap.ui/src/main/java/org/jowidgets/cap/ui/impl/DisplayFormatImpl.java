89799d85489b60b857247d5f54e32cc775d0a174
herr.grossmann <herr.grossmann@gmx.de>
2011-09-15 13:57:19 +0000

    implemented equals and hashCode for DisplayFormatImpl

--- core/org.jowidgets.cap.ui/src/main/java/org/jowidgets/cap/ui/impl/DisplayFormatImpl.java
+++ core/org.jowidgets.cap.ui/src/main/java/org/jowidgets/cap/ui/impl/DisplayFormatImpl.java
@@ -60,4 +60,35 @@ final class DisplayFormatImpl implements IDisplayFormat {
 		return decription;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof IDisplayFormat)) {
+			return false;
+		}
+		final IDisplayFormat other = (IDisplayFormat) obj;
+		if (id == null) {
+			if (other.getId() != null) {
+				return false;
+			}
+		}
+		else if (!id.equals(other.getId())) {
+			return false;
+		}
+		return true;
+	}
+
 }
