fc434f5ea87b2f1a9067972dac534ec890fa6331
herr.grossmann <herr.grossmann@gmx.de>
2011-11-04 11:28:43 +0000

    started with link stuff

--- core/org.jowidgets.cap.ui/src/main/java/org/jowidgets/cap/ui/impl/workbench/EntityTableView.java
+++ core/org.jowidgets.cap.ui/src/main/java/org/jowidgets/cap/ui/impl/workbench/EntityTableView.java
@@ -28,21 +28,115 @@
 
 package org.jowidgets.cap.ui.impl.workbench;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.jowidgets.api.command.IAction;
+import org.jowidgets.api.command.IActionBuilder;
+import org.jowidgets.api.command.ICommandExecutor;
+import org.jowidgets.api.command.IExecutionContext;
+import org.jowidgets.api.model.item.IMenuModel;
+import org.jowidgets.api.toolkit.Toolkit;
 import org.jowidgets.api.widgets.IContainer;
 import org.jowidgets.cap.common.api.bean.IBean;
+import org.jowidgets.cap.common.api.bean.IBeanDtoDescriptor;
+import org.jowidgets.cap.common.api.entity.IEntityLinkDescriptor;
+import org.jowidgets.cap.common.api.service.IBeanServicesProvider;
+import org.jowidgets.cap.common.api.service.IEntityService;
 import org.jowidgets.cap.ui.api.CapUiToolkit;
 import org.jowidgets.cap.ui.api.table.IBeanTableModel;
+import org.jowidgets.cap.ui.api.widgets.IBeanTable;
+import org.jowidgets.service.api.ServiceProvider;
+import org.jowidgets.tools.command.ActionBuilder;
 import org.jowidgets.tools.layout.MigLayoutFactory;
+import org.jowidgets.util.EmptyCheck;
 import org.jowidgets.workbench.api.IViewContext;
 import org.jowidgets.workbench.tools.AbstractView;
 
 public class EntityTableView extends AbstractView {
 
-	public EntityTableView(final IViewContext context, final IBeanTableModel<IBean> tableModel) {
+	public EntityTableView(
+		final IViewContext context,
+		final IBeanTableModel<IBean> tableModel,
+		final Map<String, IEntityLinkDescriptor> links) {
 		final IContainer container = context.getContainer();
 		container.setLayout(MigLayoutFactory.growingInnerCellLayout());
-		container.add(CapUiToolkit.bluePrintFactory().beanTable(tableModel), MigLayoutFactory.GROWING_CELL_CONSTRAINTS);
+		final IBeanTable<IBean> table = container.add(
+				CapUiToolkit.bluePrintFactory().beanTable(tableModel),
+				MigLayoutFactory.GROWING_CELL_CONSTRAINTS);
+		final IEntityService entityService = ServiceProvider.getService(IEntityService.ID);
+		if (entityService != null) {
+			addLinkActions(entityService, table, links);
+		}
 		tableModel.load();
+	}
+
+	private static void addLinkActions(
+		final IEntityService entityService,
+		final IBeanTable<IBean> table,
+		final Map<String, IEntityLinkDescriptor> links) {
+		boolean actionCreated = false;
+		final IMenuModel popMenu = table.getCellPopMenu();
+		for (final Entry<String, IEntityLinkDescriptor> linkEntry : links.entrySet()) {
+			final IAction linkAction = createLinkAction(entityService, linkEntry.getKey(), linkEntry.getValue());
+			if (linkAction != null) {
+				if (!actionCreated) {
+					popMenu.addSeparator();
+					actionCreated = true;
+				}
+				popMenu.addAction(linkAction);
+			}
+		}
+		if (!actionCreated) {
+			popMenu.addSeparator();
+		}
+	}
+
+	private static IAction createLinkAction(
+		final IEntityService entityService,
+		final String entityId,
+		final IEntityLinkDescriptor link) {
+
+		if (link.getLinkedTypeId() != null && link.getLinkableTypeId() != null) {
+
+			final IBeanServicesProvider linkTypeServices = entityService.getBeanServices(link.getLinkTypeId());
+			final IBeanDtoDescriptor linkableTypeDescriptor = entityService.getDescriptor(link.getLinkableTypeId());
+			final IBeanServicesProvider linkableTypeServices = entityService.getBeanServices(link.getLinkableTypeId());
+
+			if (isBeanDescriptorOk(linkableTypeDescriptor)
+				&& isLinkServicesOk(linkTypeServices)
+				&& isLinkableServicesOk(linkableTypeServices)) {
+				return createLinkAction(linkTypeServices, linkableTypeDescriptor, linkableTypeServices);
+			}
+		}
+		return null;
+	}
+
+	private static boolean isBeanDescriptorOk(final IBeanDtoDescriptor descriptor) {
+		return descriptor != null && !EmptyCheck.isEmpty(descriptor.getLabel());
+	}
+
+	private static boolean isLinkServicesOk(final IBeanServicesProvider services) {
+		return services != null && services.creatorService() != null;
+	}
+
+	private static boolean isLinkableServicesOk(final IBeanServicesProvider services) {
+		return services != null && services.readerService() != null;
+	}
+
+	private static IAction createLinkAction(
+		final IBeanServicesProvider linkTypeServices,
+		final IBeanDtoDescriptor linkableTypeDescriptor,
+		final IBeanServicesProvider linkableTypeServices) {
+		final IActionBuilder builder = new ActionBuilder();
+		builder.setText(linkableTypeDescriptor.getLabel() + " verknüpfen ...");
+		builder.setCommand(new ICommandExecutor() {
+			@Override
+			public void execute(final IExecutionContext executionContext) throws Exception {
+				Toolkit.getMessagePane().showInfo(executionContext, "Must be implemented!");
+			}
+		});
+		return builder.build();
 	}
 
 }
