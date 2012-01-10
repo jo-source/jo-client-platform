3818672c57a2ba27d8e3c3ea274a4028e6e97ad2
developer <developer@benjamin-marstaller.de>
2012-01-10 09:45:08 +0000

    Generate UpperIndex - DDL - String

--- util/org.jowidgets.cap.util.ddl/src/main/java/org/jowidgets/cap/util/ddl/IndexDdlCreator.java
+++ util/org.jowidgets.cap.util.ddl/src/main/java/org/jowidgets/cap/util/ddl/IndexDdlCreator.java
@@ -28,10 +28,16 @@
 
 package org.jowidgets.cap.util.ddl;
 
+import java.lang.reflect.Field;
+
+import javax.persistence.Column;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.JoinColumn;
 import javax.persistence.Persistence;
-import javax.persistence.PersistenceUtil;
+import javax.persistence.Table;
 import javax.persistence.metamodel.EntityType;
+
+import org.jowidgets.cap.service.jpa.api.ddl.UpperIndex;
 
 public final class IndexDdlCreator {
 
@@ -39,16 +45,43 @@ public final class IndexDdlCreator {
 
 	public static String createIndexDdl(final String persistenceUnitName) {
 		final StringBuilder builder = new StringBuilder();
-		@SuppressWarnings("unused")
-		final PersistenceUtil persistenceUtil = Persistence.getPersistenceUtil();
 
 		final EntityManagerFactory emf = Persistence.createEntityManagerFactory(persistenceUnitName);
 
 		for (final EntityType<?> entityType : emf.getMetamodel().getEntities()) {
-			builder.append(entityType.getName() + "\n");
+			final Class<?> javaType = entityType.getJavaType();
+			final Table table = javaType.getAnnotation(Table.class);
+			final String tableName = table.name();
+			final Field[] fields = javaType.getDeclaredFields();
+			for (final Field field : fields) {
+				final UpperIndex annotationUpper = field.getAnnotation(UpperIndex.class);
+				if (null != annotationUpper) {
+					final Column column = field.getAnnotation(Column.class);
+					String columnName = null;
+					if (null != column) {
+						columnName = column.name();
+
+					}
+					else {
+						final JoinColumn joinColumn = field.getAnnotation(JoinColumn.class);
+						if (joinColumn != null) {
+							columnName = joinColumn.name();
+						}
+					}
+					if (columnName != null) {
+						final boolean unique = annotationUpper.unique();
+						final String indexName = annotationUpper.name();
+						builder.append("CREATE ");
+						if (unique) {
+							builder.append("UNIQUE ");
+						}
+						builder.append("INDEX " + indexName + " ON " + tableName + "(UPPER(" + columnName + "));\n");
+					}
+				}
+
+			}
 		}
 
 		return builder.toString();
 	}
-
 }
