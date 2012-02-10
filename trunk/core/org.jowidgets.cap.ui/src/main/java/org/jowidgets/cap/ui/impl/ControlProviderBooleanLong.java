de767ad8e30fafd0beaef8fb701ca3ea4381eddd
herr.grossmann@gmx.de <herr.grossmann@gmx.de@241b2d79-ac52-3d8b-96e9-93e4125b0f3f>
2012-02-10 15:43:29 +0000

    use combo boxes for primitive booleans instead of checkboxes by default (checkboxes of swt looks and behaves weird. If this is desired anyway, a control panel provider can be registered for boolean.class at the InputControlSupportRegistry)

--- trunk/core/org.jowidgets.cap.ui/src/main/java/org/jowidgets/cap/ui/impl/ControlProviderBooleanLong.java
+++ trunk/core/org.jowidgets.cap.ui/src/main/java/org/jowidgets/cap/ui/impl/ControlProviderBooleanLong.java
@@ -36,6 +36,10 @@ import org.jowidgets.cap.ui.api.control.IDisplayFormat;
 
 final class ControlProviderBooleanLong extends AbstractControlProviderBoolean {
 
+	ControlProviderBooleanLong(final boolean nullable) {
+		super(nullable);
+	}
+
 	@Override
 	public IDisplayFormat getDisplayFormat() {
 		return DisplayFormat.LONG;
