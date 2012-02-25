e3d67fdc163be7c9ea7804f283a52d4340e37928
herr.grossmann <herr.grossmann@gmx.de>
2012-02-25 11:03:47 +0000

    moved SwingOptions to common module

--- samples/sample2/app-starter/org.jowidgets.cap.sample2.starter.standalone.swing/src/main/java/org/jowidgets/cap/sample2/starter/standalone/swing/Sample2StarterStandaloneSwing.java
+++ samples/sample2/app-starter/org.jowidgets.cap.sample2.starter.standalone.swing/src/main/java/org/jowidgets/cap/sample2/starter/standalone/swing/Sample2StarterStandaloneSwing.java
@@ -31,7 +31,7 @@ package org.jowidgets.cap.sample2.starter.standalone.swing;
 import javax.swing.UIManager;
 
 import org.jowidgets.cap.sample2.app.ui.workbench.Sample2Workbench;
-import org.jowidgets.spi.impl.swing.options.SwingOptions;
+import org.jowidgets.spi.impl.swing.common.options.SwingOptions;
 import org.jowidgets.workbench.impl.WorkbenchRunner;
 
 public final class Sample2StarterStandaloneSwing {
