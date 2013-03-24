3c9329045025d68ed4afbb02860e76049ec70209
herr.grossmann@gmx.de <herr.grossmann@gmx.de@241b2d79-ac52-3d8b-96e9-93e4125b0f3f>
2013-03-24 18:20:22 +0000

    implemented equals and hashcode

--- trunk/core/org.jowidgets.cap.common/src/main/java/org/jowidgets/cap/common/tools/dto/Document.java
+++ trunk/core/org.jowidgets.cap.common/src/main/java/org/jowidgets/cap/common/tools/dto/Document.java
@@ -47,4 +47,40 @@ public final class Document implements IDocument, Serializable {
 		return url;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((url == null) ? 0 : url.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Document)) {
+			return false;
+		}
+		final Document other = (Document) obj;
+		if (url == null) {
+			if (other.url != null) {
+				return false;
+			}
+		}
+		else if (!url.equals(other.url)) {
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Document [url=" + url + "]";
+	}
+
 }
