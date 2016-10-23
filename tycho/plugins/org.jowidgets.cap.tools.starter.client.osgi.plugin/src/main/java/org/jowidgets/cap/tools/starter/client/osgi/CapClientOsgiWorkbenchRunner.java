13b38d4584042dd9a198afdac02bdd9c51b4f579
Michael Grossmann <mail@herr-grossmann.de>
2016-10-23 15:33:00 +0200

    Fixed deprecation in CapClientWorkbenchRunner

--- tycho/plugins/org.jowidgets.cap.tools.starter.client.osgi.plugin/src/main/java/org/jowidgets/cap/tools/starter/client/osgi/CapClientOsgiWorkbenchRunner.java
+++ tycho/plugins/org.jowidgets.cap.tools.starter.client.osgi.plugin/src/main/java/org/jowidgets/cap/tools/starter/client/osgi/CapClientOsgiWorkbenchRunner.java
@@ -28,39 +28,97 @@
 package org.jowidgets.cap.tools.starter.client.osgi;
 
 import org.jowidgets.cap.tools.starter.client.remoting.MessagingInitializer;
-import org.jowidgets.tools.starter.classloading.ClassLoadingActivator;
 import org.jowidgets.util.Assert;
 import org.jowidgets.workbench.api.IWorkbenchFactory;
 import org.jowidgets.workbench.api.IWorkbenchRunner;
 import org.jowidgets.workbench.impl.WorkbenchRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleEvent;
+import org.osgi.framework.BundleListener;
 
-public class CapClientOsgiWorkbenchRunner extends ClassLoadingActivator {
+public class CapClientOsgiWorkbenchRunner implements BundleActivator {
 
 	private static final IWorkbenchRunner WORKBENCH_RUNNER = new WorkbenchRunner();
 
 	private final MessagingInitializer messagingInitializer;
 	private final IWorkbenchFactory workbench;
 
+	/**
+	 * Creates a new instance
+	 * 
+	 * @param workbench The workbench to run
+	 */
 	public CapClientOsgiWorkbenchRunner(final IWorkbenchFactory workbench) {
-		this(workbench, "http://localhost:8080/", new String[] {});
+		this(workbench, "http://localhost:8080/");
 	}
 
+	@Deprecated
+	/**
+	 * Creates a new CapClientOsgiWorkbenchRunner runner.
+	 * 
+	 * @param workbench The workbench to run
+	 * @param includePath The include path
+	 * 
+	 * @deprecated Do no longer use the ClassLoadingActivator, instead use the
+	 *             org.jowidgets.classloading.weaving.plugin and autostart this before any
+	 *             "Shared-Classloader-Contribution" consuming bundle has been started, e.g.
+	 *             start level 2.
+	 */
 	public CapClientOsgiWorkbenchRunner(final IWorkbenchFactory workbench, final String[] includePath) {
 		this(workbench, "http://localhost:8080/", includePath);
 	}
 
+	@Deprecated
+	/**
+	 * Creates a new CapClientOsgiWorkbenchRunner runner.
+	 * 
+	 * @param workbench The workbench to run
+	 * @param defaultServerHost The default server host
+	 * @param includePath The include path
+	 * 
+	 * @deprecated Do no longer use the ClassLoadingActivator, instead use the
+	 *             org.jowidgets.classloading.weaving.plugin and autostart this before any
+	 *             "Shared-Classloader-Contribution" consuming bundle has been started, e.g.
+	 *             start level 2.
+	 */
 	public CapClientOsgiWorkbenchRunner(
 		final IWorkbenchFactory workbench,
 		final String defaultServerHost,
 		final String[] includePath) {
-		super(includePath);
+		this(workbench, defaultServerHost);
+	}
+
+	/**
+	 * Creates a new CapClientOsgiWorkbenchRunner runner.
+	 * 
+	 * @param workbench The workbench to run
+	 * @param defaultServerHost The default server host
+	 */
+	public CapClientOsgiWorkbenchRunner(final IWorkbenchFactory workbench, final String defaultServerHost) {
 		Assert.paramNotNull(workbench, "workbench");
 		this.messagingInitializer = new MessagingInitializer(defaultServerHost);
 		this.workbench = workbench;
 	}
 
 	@Override
-	protected void bundleStartet() {
+	public void start(final BundleContext context) throws Exception {
+		final Bundle bundle = context.getBundle();
+		context.addBundleListener(new BundleListener() {
+			@Override
+			public void bundleChanged(final BundleEvent event) {
+				if (event.getBundle().equals(bundle) && BundleEvent.STARTED == event.getType()) {
+					bundleStartet();
+				}
+			}
+		});
+	}
+
+	@Override
+	public void stop(final BundleContext context) throws Exception {}
+
+	private void bundleStartet() {
 		messagingInitializer.initializeMessaging();
 		WORKBENCH_RUNNER.run(workbench);
 		System.exit(0);
