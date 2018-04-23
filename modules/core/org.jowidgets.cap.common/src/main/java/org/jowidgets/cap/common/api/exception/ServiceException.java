b78d22a92cee4131c042d71923cca2a6574d4849
Michael Grossmann <mail@herr-grossmann.de>
2018-04-23 14:43:15 +0200

    [issue #75] No cancel possible if all messaging threads are busy Improved handling of InterruptedException

--- modules/core/org.jowidgets.cap.common/src/main/java/org/jowidgets/cap/common/api/exception/ServiceException.java
+++ modules/core/org.jowidgets.cap.common/src/main/java/org/jowidgets/cap/common/api/exception/ServiceException.java
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2011, grossmann
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * * Redistributions of source code must retain the above copyright
@@ -12,7 +12,7 @@
  * * Neither the name of the jo-widgets.org nor the
  *   names of its contributors may be used to endorse or promote products
  *   derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -37,102 +37,102 @@ import org.jowidgets.util.exception.IUserCapableMessageException;
 
 /**
  * The base exception of all service exceptions.
- * 
- * Remark: Service exception has no cause exception because they will
- * potentially be used to notify the client layer about exceptions, and
- * not all exception classes may be available in the client layer (e.g. PersistenceException).
- * To make the information of the cause available in the client layer, the stack trace will
- * be created from this exception and its cause when this exception will be created.
+ *
+ * <p>Remark: Service exception has no cause exception because they will potentially be used to
+ * notify the client layer about exceptions, and not all exception classes may be available in the
+ * client layer (e.g. PersistenceException). To make the information of the cause available in the
+ * client layer, the stack trace will be created from this exception and its cause when this
+ * exception will be created.
  */
 public class ServiceException extends RuntimeException implements IUserCapableMessageException {
 
-	private static final long serialVersionUID = -7579908469741974763L;
+  private static final long serialVersionUID = -7579908469741974763L;
 
-	private final String userMessage;
-	private final String stackTrace;
+  private final String userMessage;
+  private final String stackTrace;
 
-	public ServiceException() {
-		this(null, null, null);
-	}
+  public ServiceException() {
+    this(null, null, null);
+  }
 
-	public ServiceException(final Throwable cause) {
-		this(null, null, cause);
-	}
+  public ServiceException(final Throwable cause) {
+    this(null, null, cause);
+  }
 
-	public ServiceException(final String message) {
-		this(message, null, null);
-	}
+  public ServiceException(final String message) {
+    this(message, null, null);
+  }
 
-	public ServiceException(final String message, final Throwable cause) {
-		this(message, null, cause);
-	}
+  public ServiceException(final String message, final Throwable cause) {
+    this(message, null, cause);
+  }
 
-	public ServiceException(final String message, final String userMessage) {
-		this(message, userMessage, null);
-	}
+  public ServiceException(final String message, final String userMessage) {
+    this(message, userMessage, null);
+  }
 
-	/**
-	 * Creates a new ServiceException.
-	 * 
-	 * @param message The (technical) message of the exception
-	 * 
-	 * @param userMessage The (non technical, user domain specific) message that can be presented to the end user
-	 * 
-	 * @param cause The cause of the exception. Remark: The cause will not be returned with getCause(), instead the cause
-	 *            will be used to create a complete stack trace for this exception on creation time.
-	 */
-	public ServiceException(final String message, final String userMessage, final Throwable cause) {
-		super(message);
-		this.userMessage = userMessage;
+  /**
+   * Creates a new ServiceException.
+   *
+   * @param message The (technical) message of the exception
+   * @param userMessage The (non technical, user domain specific) message that can be presented to
+   *     the end user
+   * @param cause The cause of the exception. Remark: The cause will not be returned with
+   *     getCause(), instead the cause will be used to create a complete stack trace for this
+   *     exception on creation time.
+   */
+  public ServiceException(final String message, final String userMessage, final Throwable cause) {
+    super(message);
+    this.userMessage = userMessage;
 
-		final StringBuilder stackTraceBuilder = new StringBuilder();
-		final StringWriter stringWriter = new StringWriter();
-		super.printStackTrace(new PrintWriter(stringWriter));
-		stackTraceBuilder.append(stringWriter.toString());
-		if (cause != null) {
-			stackTraceBuilder.append("Caused by: ");
-			stackTraceBuilder.append(getStackTrace(cause));
-		}
-		stackTrace = stackTraceBuilder.toString();
-	}
+    final StringBuilder stackTraceBuilder = new StringBuilder();
+    final StringWriter stringWriter = new StringWriter();
+    super.printStackTrace(new PrintWriter(stringWriter));
+    stackTraceBuilder.append(stringWriter.toString());
+    if (cause != null) {
+      stackTraceBuilder.append("Caused by: ");
+      stackTraceBuilder.append(getStackTrace(cause));
+    }
+    stackTrace = stackTraceBuilder.toString();
+  }
 
-	@Override
-	public final String getUserMessage() {
-		return userMessage;
-	}
+  @Override
+  public final String getUserMessage() {
+    return userMessage;
+  }
 
-	/**
-	 * Gets the stack trace.
-	 * 
-	 * Remark: A ServiceException has no cause. When setting a cause on creation of a service exception,
-	 * the cause will be used to create the stack trace from.
-	 * 
-	 * @return The stack trace
-	 */
-	public final String getStackTraceString() {
-		return stackTrace;
-	}
+  /**
+   * Gets the stack trace.
+   *
+   * <p>Remark: A ServiceException has no cause. When setting a cause on creation of a service
+   * exception, the cause will be used to create the stack trace from.
+   *
+   * @return The stack trace
+   */
+  public final String getStackTraceString() {
+    return stackTrace;
+  }
 
-	@Override
-	public void printStackTrace() {
-		printStackTrace(System.err);
-	}
+  @Override
+  public void printStackTrace() {
+    printStackTrace(System.err);
+  }
 
-	@Override
-	public void printStackTrace(final PrintStream stream) {
-		Assert.paramNotNull(stream, "stream");
-		stream.println(getStackTraceString());
-	}
+  @Override
+  public void printStackTrace(final PrintStream stream) {
+    Assert.paramNotNull(stream, "stream");
+    stream.println(getStackTraceString());
+  }
 
-	@Override
-	public void printStackTrace(final PrintWriter writer) {
-		Assert.paramNotNull(writer, "writer");
-		writer.println(getStackTraceString());
-	}
+  @Override
+  public void printStackTrace(final PrintWriter writer) {
+    Assert.paramNotNull(writer, "writer");
+    writer.println(getStackTraceString());
+  }
 
-	private static String getStackTrace(final Throwable throwable) {
-		final StringWriter stringWriter = new StringWriter();
-		throwable.printStackTrace(new PrintWriter(stringWriter));
-		return stringWriter.toString();
-	}
+  private static String getStackTrace(final Throwable throwable) {
+    final StringWriter stringWriter = new StringWriter();
+    throwable.printStackTrace(new PrintWriter(stringWriter));
+    return stringWriter.toString();
+  }
 }
